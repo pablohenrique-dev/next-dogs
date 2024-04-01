51caa7f15ddcdfda1a28653df60b85510ea9a3ee
Pablo Henrique <pablohdsouza.dev@gmail.com>
2024-04-01 16:44:57 -0300

    feat: add twMerge on button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,12 +1,17 @@
-interface ButtonProps extends React.ComponentProps<"button"> {}
+import { twMerge } from "tailwind-merge";
 
-export function Button({ children, ...props }: ButtonProps) {
+interface ButtonProps extends React.ComponentProps<"button"> {
+  isSubmitting?: boolean;
+}
+
+export function Button({ children, isSubmitting, ...props }: ButtonProps) {
+  const className = twMerge(
+    `mb-2 mt-2 rounded bg-primary py-4 font-bold uppercase text-primary-dark transition hover:bg-primary-medium ${isSubmitting && "cursor-not-allowed opacity-80"}`,
+    props.className,
+  );
   return (
-    <button
-      className="mb-2 mt-4 rounded bg-primary py-4 font-bold uppercase text-primary-dark transition hover:bg-primary-medium"
-      {...props}
-    >
-      {children}
+    <button {...props} className={className}>
+      {isSubmitting ? "Entrando" : children}
     </button>
   );
 }
