80e74b60efc714c85e55a724f10e2aa04e43b5b8
Pablo Henrique <pablohdsouza.dev@gmail.com>
2024-04-03 16:16:44 -0300

    feat: add forgot-password-form to forgot password page

--- src/app/account/forgot-password/page.tsx
+++ src/app/account/forgot-password/page.tsx
@@ -1,34 +1,27 @@
-import { Button } from "@/components/button";
-import { Input } from "@/components/form/input";
+import { ForgotPasswordForm } from "@/components/form/forgot-password-form";
 import { HeadingDetail } from "@/components/heading-detail";
 import Link from "next/link";
 
 export default function ForgetPasswordPage() {
   return (
-    <>
-      <div className="flex min-h-screen min-w-fit max-w-[342px] animate-fade-left flex-col justify-center px-6 sm:px-16 md:px-28 md:py-16">
-        <Link href="/" className="font-body text-base font-medium text-neutral">
-          ← Voltar para a página inicial
-        </Link>
+    <div className="flex min-h-screen min-w-fit max-w-[342px] animate-fade-left flex-col justify-center px-6 sm:px-16 md:px-28 md:py-16">
+      <Link href="/" className="font-body text-base font-medium text-neutral">
+        ← Voltar para a página inicial
+      </Link>
 
-        <div className="mt-16">
-          <h2 className="relative mb-9 font-heading text-4xl">
-            Esqueceu sua senha?
-            <HeadingDetail />
-          </h2>
+      <div className="mt-16">
+        <h2 className="relative mb-6 font-heading text-4xl">
+          Esqueceu sua senha?
+          <HeadingDetail />
+        </h2>
 
-          <form className="flex flex-col gap-6">
-            <Input
-              label="E-mail ou nome de usuário"
-              name="username-or-email"
-              placeholder="Digite o seu e-mail ou nome de usuário"
-              required
-            />
+        <p className="mb-8 w-fit text-base text-neutral md:w-[400px]">
+          Ao preencher o campo abaixo e clicar no botão enviar, será enviado
+          para o seu e-mail as instruções para redefinir sua senha.
+        </p>
 
-            <Button type="submit">Entrar</Button>
-          </form>
-        </div>
+        <ForgotPasswordForm />
       </div>
-    </>
+    </div>
   );
 }
