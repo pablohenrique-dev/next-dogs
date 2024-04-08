31ad106926e90c841407e0d6fe1686c1d11edcf7
Pablo Henrique <pablohdsouza.dev@gmail.com>
2024-04-08 16:03:57 -0300

    chore: add Photo interface to global.d.ts

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -3,3 +3,15 @@ export interface User {
   user_nicename: string;
   user_display_name: string;
 }
+
+export interface Photo {
+  id: number;
+  author: string;
+  title: string;
+  date: string;
+  src: string;
+  peso: string;
+  idade: string;
+  acessos: string;
+  total_comments: string;
+}
