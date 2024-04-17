950b293d46fbe5076ed8e3a3d6e02cb4813d4907
Pablo Henrique <pablohdsouza.dev@gmail.com>
2024-04-17 16:21:32 -0300

    refactor: change state and events to control hover by tailwind group utility class

--- src/components/photo/photo-item.tsx
+++ src/components/photo/photo-item.tsx
@@ -1,11 +1,8 @@
-"use client";
-
-import Image from "next/image";
-import React from "react";
 import { Eye } from "../icon/eye";
 import { Comment } from "../icon/comment";
 import Link from "next/link";
 import { Photo } from "@/@types/global";
+import { ImageWithSkeleton } from "../image-with-skeleton";
 
 interface PhotoItemProps {
   photo: Photo;
@@ -13,35 +10,26 @@ interface PhotoItemProps {
 }
 
 export function PhotoItem({ photo, photoIndex }: PhotoItemProps) {
-  const [isHovered, setIsHovered] = React.useState(false);
-
   return (
-    <li
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      key={photo.id}
-      className="relative overflow-hidden rounded"
-    >
+    <li key={photo.id} className="group relative overflow-hidden rounded">
       <Link href={`/photo/${photo.id}`} scroll={false}>
-        {isHovered && (
-          <div className="absolute left-0 top-0 flex h-full w-full items-end justify-center gap-4 bg-gradient-to-t from-[#000000ac] p-6">
-            <span className="flex animate-fade-top items-center gap-2 font-body text-base text-white">
-              <Eye color="white" />
-              {Number(photo.acessos).toLocaleString("pt-BR")}
-            </span>
-            <span className="flex animate-fade-top items-center gap-2 font-body text-base text-white">
-              <Comment color="white" />
-              {Number(photo.total_comments).toLocaleString("pt-BR")}
-            </span>
-          </div>
-        )}
+        <div className="absolute left-0 top-0 flex h-full w-full items-end justify-center gap-4 bg-gradient-to-t from-[#000000ac] p-6 opacity-0 transition-all group-hover:opacity-100">
+          <span className="flex animate-fade-top items-center gap-2 font-body text-base text-white">
+            <Eye color="white" />
+            {Number(photo.acessos).toLocaleString("pt-BR")}
+          </span>
+          <span className="flex animate-fade-top items-center gap-2 font-body text-base text-white">
+            <Comment color="white" />
+            {Number(photo.total_comments).toLocaleString("pt-BR")}
+          </span>
+        </div>
 
-        <Image
+        <ImageWithSkeleton
           src={photo.src}
           alt={photo.title}
           width={506}
           height={506}
-          sizes="70vw"
+          sizes="50vw"
           priority={photoIndex < 4}
         />
       </Link>
