cc21e316e9d8b0d93af8b66bc224913e638b4b4b
Pablo Henrique <pablohdsouza.dev@gmail.com>
2024-04-19 10:52:00 -0300

    refactor: change getPhoto by photoGet utility function

--- src/app/photo/[id]/page.tsx
+++ src/app/photo/[id]/page.tsx
@@ -1,68 +1,20 @@
-import { Photo } from "@/@types/global";
 import { PhotoContent } from "@/components/photo/photo-content";
-import { PHOTO_GET } from "@/services/api";
-import { handleApiError } from "@/utils/handle-errors";
+import { photoGet } from "@/utils/photo-get";
 import { notFound } from "next/navigation";
 
 interface PhotoPageParams {
   params: { id: string };
 }
 
-export interface Comment {
-  comment_ID: string;
-  comment_post_ID: string;
-  comment_author: string;
-  comment_author_email: string;
-  comment_author_url: string;
-  comment_author_IP: string;
-  comment_date: string;
-  comment_date_gmt: string;
-  comment_content: string;
-  comment_karma: string;
-  comment_approved: string;
-  comment_agent: string;
-  comment_type: string;
-  comment_parent: string;
-  user_id: string;
-}
-
-export interface PhotoContent {
-  photo: Photo;
-  comments: Comment[];
-}
-
-const REVALIDATE_TIME_IN_SECONDS = 60;
-
-async function getPhoto(id: string) {
-  try {
-    const { url, method } = PHOTO_GET();
-    const response = await fetch(url + `/${id}`, {
-      method,
-      next: {
-        tags: ["photo"],
-        revalidate: REVALIDATE_TIME_IN_SECONDS,
-      },
-    });
-    if (!response.ok) {
-      throw new Error("Ocorreu um erro ao buscar a image.");
-    }
-
-    const data = (await response.json()) as PhotoContent;
-    return { ok: true, data, error: null };
-  } catch (error) {
-    return handleApiError(error);
-  }
-}
-
 export default async function PhotoPage({ params }: PhotoPageParams) {
-  const { data } = await getPhoto(params.id);
+  const { data } = await photoGet(params.id);
 
   if (!data) return notFound();
-  return <PhotoContent photoContent={data} />;
+  return <PhotoContent photoContent={data} isModal={false} />;
 }
 
 export async function generateMetadata({ params }: PhotoPageParams) {
-  const { data } = await getPhoto(params.id);
+  const { data } = await photoGet(params.id);
 
   if (!data) return { title: "Fotos" };
   return {
