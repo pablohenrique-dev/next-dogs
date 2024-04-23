f6ff70d621d694e8c13343c67f77b9ea4c537b7d
Pablo Henrique <pablohdsouza.dev@gmail.com>
2024-04-23 11:19:34 -0300

    style: add classes for responsiveness

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -12,11 +12,14 @@ export default function GlobalError({
   return (
     <html>
       <body>
-        <div className="container flex h-screen w-screen flex-col items-center justify-center gap-6"></div>
-        <h1 className="text-5xl font-semibold">
-          Ops! Um erro inesperado ocorreu.
-        </h1>
-        <Button onClick={() => reset()}>Tentar novamente</Button>
+        <div className="container flex h-screen w-screen flex-col items-center justify-center gap-6">
+          <h1 className="text-center text-3xl font-semibold leading-[130%] md:text-5xl">
+            Ops! Um erro inesperado ocorreu.
+          </h1>
+          <Button className="px-4" onClick={() => reset()}>
+            Tentar novamente
+          </Button>
+        </div>
       </body>
     </html>
   );
